8a4a78e8db466a58b7d19207cdfe14f3d649b18d
Naveen S R <87979131+NaveenMSE@users.noreply.github.com>
2024-04-26 01:19:24 +0530

    Update Optimizer.py

--- pymarkowitz/Optimizer.py
+++ pymarkowitz/Optimizer.py
@@ -33,7 +33,7 @@ from .Metrics import MetricGenerator as MetGen
 
 class Optimizer:
 
-    def __init__(self, ret_data, moment_data, beta_data=None, asset_names=None):
+    def __init__(self, ret_data, moment_data, beta_data=None, asset_names,level1_assets,level2A_assets,level2B_assets):
         """
         Initializes an Optimizer instance with data
 
