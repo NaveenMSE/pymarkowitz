13fba254ceebca1b27daf1904b5a6310c658da3c
Naveen S R <87979131+NaveenMSE@users.noreply.github.com>
2024-04-26 07:55:58 +0530

    Update Constraints.py

--- pymarkowitz/Constraints.py
+++ pymarkowitz/Constraints.py
@@ -6,7 +6,7 @@ from .Metrics import *
 
 class ConstraintGenerator(MetricGenerator):
 
-    def __init__(self, ret_vec, moment_mat, moment, assets, beta_vec, level1_assets, level2A_assets, level2B_assets):
+    def __init__(self, ret_vec, moment_mat, moment, assets, beta_vec):
         """
         Initialize a ConstraintGenerator class with parameters to construct constraints
         Parameters are identical to its parent class MetricGenerator
